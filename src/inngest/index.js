6c15b0d3c44ecc064a5fd09e1f127bbdf0ea6723
Vineeth <vineethkumar1607@gmail.com>
2026-02-02 15:51:35 +0530

    feat: set up Inngest client, functions registry, and /api/inngest endpoint

--- src/inngest/index.js
+++ src/inngest/index.js
@@ -1,13 +1,13 @@
 import { Inngest } from "inngest";
+import { userCreation, userUpdation, userDeletion } from "./users.js";
 
-// Create a client to send and receive events
+// client to send and receive events
 export const inngest = new Inngest({ id: "project-management" });
 
 //  inngest function to save the user data to the database
-
-
-
-// Create an empty array where we'll export future Inngest functions
+// All inngest functions registered
 export const functions = [
-
+    userCreation,
+    userUpdation,
+    userDeletion,
 ];
--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ import cors from "cors";
 import dotenv from "dotenv";
 import { clerkMiddleware } from '@clerk/express'
 import { serve } from "inngest/express";
-import { inngest, functions } from "./inngest/index"
+import { inngest, functions } from "./inngest/index.js"
 
 // environment variables
 dotenv.config();
@@ -13,7 +13,11 @@ const app = express();
 // Middlewares for cors, json and cookies
 app.use(cors());
 app.use(express.json());
-// app.use(cookieParser());
+
+// Clerk middleware
+// - Verifies Clerk authentication
+// - Adds auth info to req (req.auth, req.userId, etc.)
+// - Used for protected routes in the app
 app.use(clerkMiddleware())
 
 
@@ -22,8 +26,18 @@ app.use(clerkMiddleware())
 app.get("/", (req, res) => {
   res.json({ message: "Server is running successfully!" });
 });
-// Set up the "/api/inngest" (recommended) routes with the serve handler
-app.use("/api/inngest", serve({ client: inngest, functions }));
+
+/*
+This route is used by Inngest:
+- Discover all registered background functions
+- Execute them when events occur (ex: clerk/user.created)
+- Handle retries, failures, and step execution
+*/
+
+app.use("/api/inngest", serve({
+  client: inngest,          // Inngest client instance
+  functions                   // Array of all registered Inngest functions
+}));
 
 const PORT = process.env.PORT || 5000;
 
